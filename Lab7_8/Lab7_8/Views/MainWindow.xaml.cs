24ce9a16660f215da171ba1e08e506eef7f4151d
Vasilevskiy Pasha <vasilevskij00@inbox.ru>
2020-04-16 17:49:11 +0300

    feat: add localization + icon + datagrid style

--- Lab7_8/Lab7_8/Views/MainWindow.xaml.cs
+++ Lab7_8/Lab7_8/Views/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Lab7_8.ViewModels;
 using System;
+using System.Globalization;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace Lab7_8
@@ -15,6 +17,48 @@ namespace Lab7_8
             InitializeComponent();
             this.Cursor = new Cursor($"{Environment.CurrentDirectory}\\Arrow.cur");
             DataContext = new MainViewModel(this);
+
+            App.LanguageChanged += LanguageChanged;
+
+            CultureInfo currLang = App.Language;
+
+            //Заполняем меню смены языка:
+            menuLanguage.Items.Clear();
+            foreach (var lang in App.Languages)
+            {
+                MenuItem menuLang = new MenuItem();
+                menuLang.Header = lang.DisplayName;
+                menuLang.Tag = lang;
+                menuLang.IsChecked = lang.Equals(currLang);
+                menuLang.Click += ChangeLanguageClick;
+                menuLanguage.Items.Add(menuLang);
+            }
+        }
+
+        private void LanguageChanged(Object sender, EventArgs e)
+        {
+            CultureInfo currLang = App.Language;
+
+            //Отмечаем нужный пункт смены языка как выбранный язык
+            foreach (MenuItem i in menuLanguage.Items)
+            {
+                CultureInfo ci = i.Tag as CultureInfo;
+                i.IsChecked = ci != null && ci.Equals(currLang);
+            }
+        }
+
+        private void ChangeLanguageClick(Object sender, EventArgs e)
+        {
+            MenuItem mi = sender as MenuItem;
+            if (mi != null)
+            {
+                CultureInfo lang = mi.Tag as CultureInfo;
+                if (lang != null)
+                {
+                    App.Language = lang;
+                }
+            }
+
         }
     }
 }
